0a789cca20e07ab50b080c9844160c0d0044e6dc
Chubbs91 <chaskin1991@outlook.com>
2022-02-10 14:27:21 -0600

    get routes created for HTMLroutes

---
+++ routes/htmlroutes.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+// This line means this file has been exported as a function
+module.exports = function(app) {
+    
+    // HTML Get requests
+    // Code will handle when a user visits the page.
+    app.get('./notes', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+    // If no matching route found.  default to index
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+};
