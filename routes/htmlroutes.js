19e52aad6b5ca5ac7441598f4d6e9e4585514d96
Chubbs91 <chaskin1991@outlook.com>
2022-02-11 22:37:03 -0600

    fixed error.  removed 1 period from html routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
     
     // HTML Get requests
     // Code will handle when a user visits the page.
-    app.get('./notes', function (req, res) {
+    app.get('/notes', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
